9b1dc8d4c9a5a8f206ab137f7be05e07b12e23a0
DanRivalle <dan.santos.hd@outlook.com>
2022-11-14 18:44:08 -0300

    Se avanza un poco la funcion ann_train_pso

--- pso.py
+++ pso.py
@@ -2,6 +2,10 @@ import numpy as np
 from my_utility import get_mse
 from ann import forward
 
+# Constantes
+C1 = 1.05
+C2 = 2.95
+
 def init_weights(m, L, K):
 	r1 = np.sqrt(6 / (L + m))
 	r2 = np.sqrt(6 / (K + L))
@@ -10,6 +14,7 @@ def init_weights(m, L, K):
 	v = np.random.rand(K, L) * 2 * r2 - r2
 	
 	return w, v
+
 
 def init_swarm(m, L, K, Np):
 	swarm = []
@@ -20,43 +25,67 @@ def init_swarm(m, L, K, Np):
 
 	return np.array(swarm)
 
+
 def init_pso(m, L, K, Np):
 	S = init_swarm(m, L, K, Np)
 	P = np.zeros(S.shape)
 	Pg = np.zeros(S.shape[1])
-	P_fit = np.zeros(Np)
-	Pg_fit = 0
+	P_fit = np.zeros(Np)		# TODO: Inicializar fitness
+	Pg_fit = P_fit[0]
 
 	return S, P, Pg, P_fit, Pg_fit
+
 
 def extract_weights(particle, m, L, K):
 	w = particle[:m * L].reshape((L, m))
 	v = particle[m * L:].reshape((K, L))
 	return w, v
 
-def get_alpha(current_iter, max_iter, a_min=0.1, a_max=0.95):
+
+def get_alpha(max_iter, current_iter, a_min=0.1, a_max=0.95):
 	a = a_max - ((a_max - a_min) / max_iter) * current_iter
 	return a
 
-def swarm_fitness(X, y_true, m, L, K, H, f):
+
+def ann_fitness(S):
 	pass
 
-def particle_fitness(particle, X, y_true, m, L, K, H, f):
-	w, v = extract_weights(particle, m, L, K)
-	y_predict = forward(X, w, v, H, f)
 
-	mse = get_mse(y_predict, y_true)
-	return mse 
+def update_fitness(S, S_fit, P, P_fit, Pg, Pg_fit, Np):
+	i_min = 0
+	for i in range(Np):
+		if S_fit[i] < P_fit[i]:
+			P[i] = S[i].copy()
+			P_fit[i] = S_fit[i]
+		
+		if S_fit[i] < S_fit[i_min]:
+			i_min = i
 
-def update_fitness():
-	pass
+	if S_fit[i_min] < Pg_fit:
+		Pg = S[i_min].copy()
+		Pg_fit = S_fit[i_min]
 
-def update_veloc():
-	pass
+	return P, P_fit, Pg, Pf_fit
+
+
+def update_veloc(S, V, P, Pg, max_iter, current_iter):
+	a = get_alpha(max_iter, current_iter)
+	r1 = np.random.rand()
+	r2 = np.random.rand()
+
+	V_k = a*V + C1*r1*(S - P) + C2*r2*(S - Pg)
+	return V_k
+
 
 def ann_train_pso(m, L, K, Np, max_iter):
 	S, P, pg, P_fit, Pg_fit = init_pso(m, L, K, Np)
 
 	for i in range(max_iter):
-		pass
+		S_fit = ann_fitness(S)
+		P, P_fit, Pg, Pg_fit = update_fitness(S, S_fit, P, P_fit, Pg, Pg_fit, Np)
+		V = update_veloc(S, V, P, Pg, max_iter, current_iter)
+		S = S + V
 		
+	w, v = extract_weights(Pg, m, L, K)
+	return w, v
+
