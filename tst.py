4e153fcdc9a3cffa197a66cd86a4d5b42fd5bccb
Patricio Labra <patricio.labra827@gmail.com>
2022-11-19 16:33:45 -0300

    se eliminan unas basuras jkdsa

--- tst.py
+++ tst.py
@@ -1,8 +1,6 @@
 import numpy as np
 from metrics import get_metrics, get_confusion_matrix
 
-f1_scores = []
-y_predict = np.array([[1,0,0], [0,0,1],[0,0,1],[1,0,0], [0,1,0],[0,0,1]]).T
-y_test = np.array([[1,0,0], [0,0,1],[0,1,0],[1,0,0], [1,0,0], [0,1,0]]).T
+
 
 
