00740727e1adf3db86bb1526c823f38b541e73b9
Dhereck1 <82067086+Dhereck1@users.noreply.github.com>
2022-11-19 22:05:49 -0300

    Update trn.py

--- trn.py
+++ trn.py
@@ -4,7 +4,7 @@ from my_utility import *
 from pso import *
 from pso import ann_train_pso
 
-# Se carga la configuracion
+# Se carga la configuracion de PSO y BP, junto con cargar datos dtrn y dtst 
 cnf = readConfigAndData()
 
 #Se realiza PSO
@@ -16,7 +16,6 @@ np.savetxt("costo_pso.csv", np.array(pso_MSE))
 w, v, ann_MSE = ann_train(w, v, cnf)
 #Se guarda la lista de los MSE de la ANN por iteración
 np.savetxt("costo_gd.csv", np.array(ann_MSE))
-print(min(ann_MSE) , max(ann_MSE))
 
 #Se guardan los pesos de la capa oculta(w) y de la capa de salida(v)
 np.savez("pesos.npz", w, v)
